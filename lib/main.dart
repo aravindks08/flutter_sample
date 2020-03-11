dc046d530c706a53ed73d125496b37a4ff2035b2
Aravind <aravind.ks@rakuten.com>
2020-03-11 13:54:22 +0530

    maintainance modal

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Aravind'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
+  int _counter = 0;
+  @override
+  void initState() {
+    print("hello1");
+    print(this._counter);
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('state = $state');
+    showSimpleCustomDialog(context);
+    print(this._counter);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                showSimpleCustomDialog(context);
+              },
+              child: Text("Show Simple Custom Dialog"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void showSimpleCustomDialog(BuildContext context) {
+    Dialog simpleDialog = Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Container(
+        height: 300.0,
+        width: 300.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Maintainance mode : 503',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      exit(0);
+                      SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                    },
+                    child: Text(
+                      'Okay',
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => simpleDialog);
+}
+
+// class LifecycleEventHandler extends WidgetsBindingObserver {
+//   final AsyncCallback resumeCallBack;
+//   final AsyncCallback suspendingCallBack;
+
+//   LifecycleEventHandler({
+//     this.resumeCallBack,
+//     this.suspendingCallBack,
+//   });
+
+//   @override
+//   Future<Null> didChangeAppLifecycleState(AppLifecycleState state) async {
+//     switch (state) {
+//       case AppLifecycleState.resumed:
+//         if (resumeCallBack != null) {
+//           await resumeCallBack();
+//         }
+//         break;
+//       case AppLifecycleState.inactive:
+//       case AppLifecycleState.paused:
+//       case AppLifecycleState.detached:
+//         if (suspendingCallBack != null) {
+//           await suspendingCallBack();
+//         }
+//         break;
+//     }
+//   }
+// }
